3217cdd5d41cbb1f119bfee89fe61b1f04715bd5
DELL <DELL@DESKTOP-06MP6T8>
2021-07-08 04:08:06 +0530

    added class Type Casting.java

---
+++ Type Casting/src/TypeCasting.java
@@ -0,0 +1,16 @@
+
+public class TypeCasting {
+	public static void main(String[] args) {
+	byte b=10;
+	byte c=2;
+	int myint=b*c;
+	System.out.println(myint);
+	byte d=(byte)(b*c);
+	System.out.println(d);
+	int myintvalue=257;
+	byte mybitevalue=(byte)myintvalue;
+	System.out.println(mybitevalue);
+	}
+	}
+
+
